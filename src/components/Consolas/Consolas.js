f040489fa49cf43497cf1a9c95e5275c0ab8e156
FerNoceti <fer.j.noceti@gmail.com>
2022-04-14 13:05:03 -0300

    Consolas

--- src/components/Consolas/Consolas.js
+++ src/components/Consolas/Consolas.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ItemListContainer from '../components/ItemListContainer/ItemListContainer'
+import ItemListContainer from '../ItemListContainer/ItemListContainer'
 import './Consolas.css'
 
 function Consolas() {
